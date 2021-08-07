ba27b4cb5bf7f3dbe271390a78c7f104626081df
SlackMagiC <laurent.pietrzyk@gmail.com>
2021-08-07 11:01:21 +0200

    ✨ Add Healthcheck

--- bin/helix-user-grpc/src/main.rs
+++ bin/helix-user-grpc/src/main.rs
@@ -10,12 +10,19 @@ const APP_NAME: &str = "USER_APP";
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("[HELIX gRPC {} {}]", APP_NAME, env!("CARGO_PKG_VERSION"));
     let addr = "127.0.0.1:42420".parse()?;
+
+    let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
+    health_reporter
+        .set_serving::<UserServiceServer<ImplUserService>>()
+        .await;
+
     let impl_user_service = ImplUserService::default();
 
     print!("--> Started on ");
     println!("http://{}", addr);
 
     Server::builder()
+        .add_service(health_service)
         .add_service(UserServiceServer::new(impl_user_service))
         .serve(addr)
         .await?;
